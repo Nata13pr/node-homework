d5d85768703dc33f1918401d7c938f4c311b7cb9
nata13pr <Nata13pr@gmail.com>
2024-07-09 09:47:52 +0300

    feat:http,__dirname,__filename

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('node:http');
+
+const {sdf} = require('./some_dir/helper')
+
+sdf()
+
+console.log('Hello World!');
+console.log(__dirname);
+console.log(__filename);
+console.log(process.cwd());
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('hello world\n222')
+})
+
+server.listen(3000);
